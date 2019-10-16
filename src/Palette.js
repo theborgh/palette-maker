09e09bead9df97366023648d99f82191c3f5d24c
theborgh <borghiborghi@gmail.com>
2019-10-16 19:04:30 +0200

    Add Material UI and select menu

--- src/Palette.js
+++ src/Palette.js
@@ -8,24 +8,36 @@ export default class Palette extends Component {
     super(props);
     this.state = {
       level: 500,
+      format: 'hex',
     };
 
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeColorFormat = this.changeColorFormat.bind(this);
   }
 
   changeLevel(newLevel) {
     this.setState({level: newLevel});
   }
 
+  changeColorFormat(val) {
+    this.setState({
+      format: val,
+    });
+  }
+
   render() {
     const {colors} = this.props.palette;
-    const {level} = this.state;
+    const {level, format} = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div className='Palette'>
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeColorFormat}
+        />
         {/* Navbar goes here */}
         <div className='Palette-colors'>{colorBoxes}</div>
       </div>
