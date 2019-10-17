a0ec57600b9ff47b2d9d09ffe86c2da1626b856c
theborgh <borghiborghi@gmail.com>
2019-10-17 13:04:31 +0200

    Add dynamic Palette routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.findPalette = this.findPalette.bind(this);
+  }
+
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path='/' exact render={() => <h1>Palette list</h1>} />
+          <Route
+            path='/palette/:id'
+            exact
+            render={routeProps => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
