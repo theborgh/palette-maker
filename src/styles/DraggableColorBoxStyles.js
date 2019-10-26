2bf427948ba08eefdeca56ea848cbcf03ff7e713
theborgh <borghiborghi@gmail.com>
2019-10-26 10:04:08 +0200

    Refactor styles and add constants file

---
+++ src/styles/DraggableColorBoxStyles.js
@@ -0,0 +1,34 @@
+const styles = {
+  root: {
+    width: '20%',
+    height: '25%',
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-4px',
+
+    '&:hover svg': {
+      color: 'white',
+      transform: 'scale(1.3)',
+    },
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0',
+    bottom: '0',
+    padding: '10px',
+    color: 'rgba(0,0,0,0.5)',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  deleteIcon: {
+    transition: 'all .3s ease-in-out',
+  },
+};
+
+export default styles;
