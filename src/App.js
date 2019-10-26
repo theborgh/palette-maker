f47610d0e11fc4eebf2328f16f0e8cdb71b8e362
theborgh <borghiborghi@gmail.com>
2019-10-26 10:27:58 +0200

    Add save palettes to localstorage functionality

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+
     this.state = {
-      palettes: seedColors,
+      palettes: savedPalettes || seedColors,
     };
 
     this.findPalette = this.findPalette.bind(this);
@@ -26,8 +28,17 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    console.log(newPalette);
-    this.setState({palettes: [...this.state.palettes, newPalette]});
+    this.setState(
+      {palettes: [...this.state.palettes, newPalette]},
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem(
+      'palettes',
+      JSON.stringify(this.state.palettes)
+    );
   }
 
   render() {
